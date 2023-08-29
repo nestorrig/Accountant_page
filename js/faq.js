e3c44cd56f60c9a120e2f190aecb1d1b4376cca2
nestorrig <neriga10@hotmail.com>
2023-08-29 17:36:02 -0600

    v1.11: add animation in faq, add new logos

--- js/faq.js
+++ js/faq.js
@@ -1,20 +1,18 @@
 const questions = document.getElementsByClassName('faq__item__question')
 for (const q of questions) {
-    q.addEventListener('click', showOrHideAnswer, )
+    q.addEventListener('click', showOrHideAnswer)
 }
 
 function showOrHideAnswer(e) {
     const question = e.currentTarget;
     const answer = question.nextElementSibling
-    const arrowDown = question.children[1].firstElementChild
+    console.log(answer);
     
     question.classList.toggle('faq__item__question__active')
 
-    if (answer.classList.contains('inactive')) {
-        arrowDown.style.animation = 'rotateIcon .3s linear both'
-        answer.classList.remove('inactive')
+    if (answer.classList.contains('faq__item__answer__active')) {
+        answer.classList.remove('faq__item__answer__active')
     } else {
-        answer.classList.add('inactive')
-        arrowDown.style.animation = 'reverseRotateIcon .3s linear both'
+        answer.classList.add('faq__item__answer__active')
     }
 }
