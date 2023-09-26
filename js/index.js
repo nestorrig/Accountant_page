b9d498fb7ba04c0f6b3b8eeda1948e0e3e9437a2
nestorrig <neriga10@hotmail.com>
2023-09-26 01:07:57 -0600

    v1.15 change brightness when menu is open

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@ const openMenu = document.getElementById('OpenMenu');
 const closeMenu = document.getElementById('CloseMenu');
 const buttonsBox = document.getElementById('MenuButtons');
 const navItems = document.getElementsByClassName('menu__item__mobile');
+const main = document.querySelector('.main')
+const footer = document.querySelector('.footer')
+const content = document.querySelector('.content-container')
 
 for (const item of navItems) {
     item.addEventListener('click', showOrHideMenu);
@@ -12,13 +15,20 @@ buttonsBox.addEventListener('click', showOrHideMenu);
 function showOrHideMenu() {
     openMenu.classList.toggle('inactive')
     closeMenu.classList.toggle('inactive')
+    main.classList.toggle('brightness')
+    main.classList.toggle('blockIt')
+    footer.classList.toggle('brightness')
+    footer.classList.toggle('blockIt')
+    
     
     const test = openMenu.classList.contains('inactive')
     
     if (test) {
+        content.addEventListener('click', showOrHideMenu);
         menuMobile.style.animation = 'showMenu .3s linear both'
         closeMenu.style.animation = 'rotateIcon .3s linear both'
     } else {
+        content.removeEventListener('click', showOrHideMenu);
         menuMobile.style.animation = 'hideMenu .3s linear both'
         openMenu.style.animation = 'rotateIcon .3s linear both'
     }
